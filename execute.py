8e59a73ffcd79cc93c15034b849ec97155011f04
rongione <151832721+rongione@users.noreply.github.com>
2025-05-13 09:57:34 +0200

    Stop tracking execute.py

--- execute.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# Run the command using sudo
-os.system('sudo python3 testprogram2.py --led-rows=32 --led-cols=64 --led-gpio-mapping=adafruit-hat --led-scan-mode=1 --led-slowdown-gpio=5 --led-pwm-bits=9')
